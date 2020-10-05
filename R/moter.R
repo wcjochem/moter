64c4e53c0cb49ad0c4df1fa289e2dbe7d5119737
Chris Jochem <wcjochem@users.noreply.github.com>
2020-10-05 15:21:13 +0100

    package help doc

---
+++ R/moter.R
@@ -0,0 +1,16 @@
+#' moter: A package for creating morphological tessellations from building
+#' footprint polygons
+#'
+#' The moter package is designed to explore the morphological tessellation
+#' algorithm described by Fleischmann et al. (2020) and implemented in the
+#' Python package Momepy \url{https://docs.momepy.org}.
+#'
+#' Morphological tessellations are an approach to partitioning space based on
+#' voronoi polygons created around 2D building footprints. These resulting
+#' morphological tessellation polygons approximate a plot scale area and can be
+#' used in quantitative study of urban form and morphology.
+#'
+#' @docType package
+#' @name moter
+NULL
+#
